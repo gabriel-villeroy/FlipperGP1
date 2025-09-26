fc3395d58fe5a010f5b9f782c8b5c0838bb1a1a9
gab.nrs <villeroy.gab@gmail.com>
2025-09-26 13:58:45 +0200

    DOTween import + basic paddle tp

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using DG.Tweening;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] private GameObject RPivot;
+    [SerializeField] private GameObject LPivot;
+
+    [SerializeField] private float defaultPivotRot;
+    [SerializeField] private float movementPivotRot;
+    
+    [SerializeField]
+
+    private float pivotTargetRot;
+
+    private void Start()
+    { 
+        RPivot.transform.rotation = Quaternion.Euler(0,0,defaultPivotRot);
+        LPivot.transform.rotation = Quaternion.Euler(0,0,-defaultPivotRot);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightShift)) Pivot(RPivot, true);
+        if(Input.GetKeyDown(KeyCode.LeftShift)) Pivot(LPivot, true);
+        if(Input.GetKeyUp(KeyCode.RightShift)) Pivot(RPivot, false);
+        if(Input.GetKeyUp(KeyCode.LeftShift)) Pivot(LPivot, false);
+    }
+
+    private void Pivot(GameObject targetPivot, bool activate)
+    {
+        if (activate)
+        {
+            pivotTargetRot = movementPivotRot;
+        }
+        else
+        {
+            pivotTargetRot = defaultPivotRot;
+        }
+        
+        if (targetPivot == LPivot)
+        {
+            targetPivot.transform.rotation = 
+                Quaternion.Euler(0,0,-pivotTargetRot);
+        }
+
+        if (targetPivot == RPivot)
+        {
+            targetPivot.transform.rotation = 
+                Quaternion.Euler(0,0,pivotTargetRot);
+        }
+    }
+}
